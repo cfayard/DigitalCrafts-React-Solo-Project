f1e27605521db0757fc5363d8d48e8460190822b
Casey Fayard <fayardcasey@gmail.com>
2020-02-18 16:39:34 -0500

    new dashboard, plaidlink setup

---
+++ client/budget-app/src/components/dashboard/Accounts.js
@@ -0,0 +1,1 @@
+// Let’s create an Accounts.js file within the dashboard folder of the components directory. This component will display all linked accounts and transactions. We’ll display this component in our Dashboard only if a user has at least one account linked.
