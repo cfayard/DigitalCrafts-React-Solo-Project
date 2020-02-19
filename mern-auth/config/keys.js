1b12c3c031d08abafe2e9bb6d0513362e69636ee
Casey Fayard <fayardcasey@gmail.com>
2020-02-19 11:18:12 -0500

    updating master

---
+++ mern-auth/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: "mongodb+srv://cfayard:terminus1@new-app-cluster-ivh5k.mongodb.net/test?retryWrites=true&w=majority" ,
+  secretOrKey: "secret",
+  PLAID_CLIENT_ID: "5e44449647679a001213083e",
+  PLAID_SECRET: "8c95fe5e7e437eaa2fd98cb7062644",
+  PLAID_PUBLIC_KEY:  "c3d4359494f762298793738f10eda6"
+};
