5bf005171196935086e28b880cc23e99d8e2e5a5
Casey Fayard <fayardcasey@gmail.com>
2020-02-19 09:56:31 -0500

    setting up file to ignore API credentials

--- mern-auth/routes/api/plaid.js
+++ mern-auth/routes/api/plaid.js
@@ -7,9 +7,12 @@ const mongoose = require("mongoose");
 // Load Account and User models
 const Account = require("../../models/Account");
 const User = require("../../models/User");
-const PLAID_CLIENT_ID = "5e44449647679a001213083e";
-const PLAID_SECRET = "8c95fe5e7e437eaa2fd98cb7062644";
-const PLAID_PUBLIC_KEY = "c3d4359494f762298793738f10eda6";
+
+const keys = require("../../config/keys");
+const PLAID_CLIENT_ID = keys.PLAID_CLIENT_ID;
+console.log(PLAID_CLIENT_ID)
+const PLAID_SECRET = keys.PLAID_SECRET;
+const PLAID_PUBLIC_KEY = keys.PLAID_PUBLIC_KEY;
 const client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
